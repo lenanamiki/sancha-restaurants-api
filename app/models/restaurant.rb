1e4c8bc25d2f434d1bf40075f11d4fd8fd0d250c
Lena Namiki <lena.namiki@gmail.com>
2021-10-13 13:45:15 +0900

    Added validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,10 @@
 class Restaurant < ApplicationRecord
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :address, presence: true
+  validates :openhour, presence: true
+  validates :closehour, presence: true
+  validates :price, presence: true
+  enum price: %i[¥ ¥¥ ¥¥¥]
+  enum status: %i[pending approved declined]
 end
